f98e1ec0085222bdb0297dfe6a93cfceb99fe243
Takahiro Konno <t.konno@shishimaro.co.jp>
2023-06-22 13:29:28 +0900

    add: プロンプトテンプレートで入力メッセージを作成

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import openai
 
+from langchain.prompts import PromptTemplate
+
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     AIMessage,
@@ -13,19 +15,29 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-# 質問テンプレートの定義
+# プロンプトテンプレートの定義
 template_asking_shisha_flavor_mixes = """
 シーシャのフレーバーミックスを3個教えてください。\
-3種類のフレーバーをミックスした組み合わせで、ピーチを必ず含んでください。\
+3種類のフレーバーをミックスした組み合わせで、{flavors}を必ず含んでください。\
 各フレーバーミックスの特徴も教えてください。
 """
 
 if __name__ == '__main__':
-    # チャットモデルの作成
-    chat = ChatOpenAI(model='gpt-3.5-turbo')
+    # プロンプトテンプレートの作成
+    prompt = PromptTemplate(
+        input_variables=['flavors'],
+        template=template_asking_shisha_flavor_mixes
+    )
 
-    # テンプレートの作成
-    message = chat.predict_messages([HumanMessage(content=template_asking_shisha_flavor_mixes)])
+    # テンプレートからプロンプトを生成
+    prompt = prompt.format(flavors='アップル')
+    # print(prompt)
+
+    # チャットモデルの作成
+    llm = ChatOpenAI(model='gpt-3.5-turbo')
+
+    # チャットモデルの実行
+    message = llm.predict_messages([HumanMessage(content=prompt)])
 
     print(message.content)
     # >> AIMessage(content="J'aime programmer.", additional_kwargs={})
